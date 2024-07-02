3fd17d70590b07ef42bd4142ff87b1524b911cab
Joaquín <joaquinmendozasebastian@gmail.com>
2024-07-02 13:59:09 +0200

    creación de funciones para servir ficheros estáticos

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const {createServer} = require("http");
 const {createReadStream} = require("fs");
 
+function contentType(extension){
+    if(extension == "html") return "text/html";
+    if(extension == "css") return "text/css";
+    if(extension == "js") return "text/javascript";
+    if(extension == "jpg" || extension == "jpeg") return "image/jpeg";
+    if(extension == "png") return "image/png";
+    if(extension == "json") return "application/json";
+    return "text/plain";
+}
+
+function servirFichero(respuesta,ruta,tipo,status){
+    respuesta.writeHead(status, { "Content-type" : tipo });
+
+    let fichero = createReadStream(ruta);
+
+    fichero.pipe(respuesta);
+
+    fichero.on("end", () => {
+        respuesta.end();
+    });
+
+}
+
 const servidor = createServer((peticion,respuesta) => {
-    respuesta.writeHead(404, { "Content-type" : "text/html" });
-    respuesta.write("..x cosa");
-    respuesta.end();
+    servirFichero(respuesta,"./publico/index.html",contentType("html"),200);
 });
 
 servidor.listen(process.env.PORT || 3000);
